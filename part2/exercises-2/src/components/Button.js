81e03249a217d94a067b6fe56ab725798c178072
Madeline <moon.madeline@gmail.com>
2023-08-07 11:34:13 -0500

    Part 1 & 2

--- part2/exercises-2/src/components/Button.js
+++ part2/exercises-2/src/components/Button.js
@@ -2,11 +2,18 @@ import './styles.css';
 
 function Button() {
 
+   function onLearnMore() {
+      alert("Splish splash");
+   }
+
    return ( 
-         <button>
+      // needed to make it a JSX variable that is not automatically called
+         <button onClick={onLearnMore}> 
             Learn More
          </button>
    );
 }
+
+
 
 export default Button;

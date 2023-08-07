859144d6a663d6dbc31cc11123ef7d4878c8ea69
Madeline <moon.madeline@gmail.com>
2023-08-07 11:55:50 -0500

    Part 3

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,14 +1,32 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json';
 
+const listItems = oceans.map(ocean => {
+   return (
+      
+      <div key = {ocean.id} className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+         <img src ={ocean.image} alt ={ocean.name} className ="img" />
+         <h1>{ocean.name}</h1>
+         <h2>Fun facts</h2>
+         <ul>
+            <li key={`${ocean.id}fact-1`}>{ocean.fact1}</li>
+            <li key={`${ocean.id}fact-2`}>{ocean.fact2}</li>
+            <li key={`${ocean.id}fact-2`}>{ocean.fact3}</li>
+         </ul>
+         <Button />
+      </div>
+   );
+}
+
+   );
 
 function Profile()
 {
    return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+         <ul>
+            {listItems}
+         </ul>
    );
 }
 
